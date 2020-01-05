ba785f8461b2515c9118f6ce9253a791e112dc15
Anselm <macbook@macbooks-MacBook-Pro.local>
2020-01-05 16:42:01 +0100

    on mac edit retailer resources

--- app/Http/Resources/RetailerOrderResource.php
+++ app/Http/Resources/RetailerOrderResource.php
@@ -14,6 +14,17 @@ class RetailerOrderResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'order_id' => $this->id,
+            'product_id' => $this->product_id,
+            'quantity' => $this->quantity,
+            'unit_price' => $this->unit_price,
+            'order_amount' => $this->order_amount,
+            'created_by' => $this->created_by,
+            'is_confirmed' => $this->is_confirmed,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
+        // return parent::toArray($request);
     }
 }
