2c956531e2f6f5ad886f342ff6cf6395f1917c9d
Ajayi Adeyemi <aajayiadeyemi@gmail.com>
2020-01-14 15:54:10 +0100

    offer module updated

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -12,7 +12,7 @@ $factory->define(Product::class, function (Faker $faker) {
         "user_id" => factory(\Emrad\User::class, 1)->create()->first()->id,
         "sku" => base_convert(microtime(true), 10, 36),
         "description" => $faker->sentence(20),
-        "image" => $faker->imageUrl(450, 450),
+        "image" => "https://dummyimage.com/100x100/fff/0000",
         "size" => $faker->numberBetween(3, 10),
         "price" => $faker->numberBetween(1000, 3000)
     ];
