5363165481be9da4f2747bf278dcb009ee7916d8
Anselm <anselmleo@gmail.com>
2020-02-02 21:48:59 +0100

    add order details to order notification email view

--- resources/views/emails/new_retailer_order.blade.php
+++ resources/views/emails/new_retailer_order.blade.php
@@ -1,5 +1,18 @@
 
+<h3 class="card">Emrad New Order Notification</h3>
 <p><b>Hello Niyi</b>, </p>
 <p> A retailer just place a new order. </p>
+<p>Order Details</p>
+<table>
+    <th>Retailer Info</th><th>Product Info</th><th>Unit Price</th><th>Quantity</th><th>Total</th>
+    <tr>
+        <td>{{ $user->first_name . ' ' . $user->last_name }}</td>
+        <td>{{ $order->product_id }}</td>
+        <td>{{ $order->unit_price }}</td>
+        <td>{{ $order->quantity }}</td>
+        <td>{{ $order->order_amount }}</td>
+    </tr>
+</table>
 
-
+<p>Kind regards</p><br><br>
+<p>Emrad Team</p>
